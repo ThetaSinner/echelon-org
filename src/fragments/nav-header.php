dd395d8c2503dfdebf3886cbb8edd040bf2249bc
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-02-20 22:58:22 +0000

    add to the main page and create a source page for github links

--- src/fragments/nav-header.php
+++ src/fragments/nav-header.php
@@ -2,8 +2,11 @@
     <div class="el-nav-header-top"></div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <ul class="navbar-nav">
-            <li class="nav-item active" id="el-nav-home">
+            <li class="nav-item" id="el-nav-home">
                 <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item" id="el-nav-source">
+                <a class="nav-link" href="/pages/source.php">Source</a>
             </li>
             <li class="nav-item" id="el-nav-blog">
                 <a class="nav-link" href="/pages/blog.php">Blog</a>
---
+++ src/pages/source.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <? require_once $_SERVER['DOCUMENT_ROOT'] . '/fragments/header-fragment.php' ?>
+
+    <script type="text/javascript">
+        activateNavItemWhenReady('el-nav-source');
+    </script>
+</head>
+<body>
+    <? require_once $_SERVER['DOCUMENT_ROOT'] . '/fragments/nav-header.php' ?>
+    <div class="container">
+        <div class="border-bottom border-right border-dark mt-3 p-2 mx-5">
+            <p>
+                The compiler. This is a work in progress and was initially intended as a learning project to understand compiler technology.
+                I see it developing into the sandpit for ideas from this site.
+            </p>
+            <a href="https://github.com/ThetaSinner/echelon">Echelon</a>
+        </div>
+    </div>
+</body>
+</html>
